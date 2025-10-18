c59d2282bdc351c515d649a3af1bf52abc45e15a
Rexxu <130981329+RexxuDesu@users.noreply.github.com>
2025-10-18 23:24:54 +0800

    Fixed issue with syntax of textutils

--- lunRPentDIR/main.lua
+++ lunRPentDIR/main.lua
@@ -3,19 +3,19 @@ local function listen()
     while true do
         local targetID, targetMsg, targetProt = rednet.receive()
         if targetID then
-            print("ID: " .. targetID .. "   MSG: " .. targetMsg .. "   PRT: " .. targetProt)
+            textutils.slowPrint("ID: " .. targetID .. "   MSG: " .. targetMsg .. "   PRT: " .. targetProt)
         end
         sleep(1)
     end
 end
 
 while true do
-    textutils.slowPrint.io.write("kali@:~ ")
+    textutils.slowWrite("kali@:~ ")
     local input = read()
     if input then
         if input == "netS" then
             shell.run("clear")
-            textutils.slowPrint.io.write("Listening for any broadcast...")
+            textutils.slowWrite("Listening...")
             listen()
         end
     end
