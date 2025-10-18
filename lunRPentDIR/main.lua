6611edc06fe13f0810c2923add87b6e05fd0ccfe
Rexxu <130981329+RexxuDesu@users.noreply.github.com>
2025-10-18 23:20:24 +0800

    Slow text attempt

--- lunRPentDIR/main.lua
+++ lunRPentDIR/main.lua
@@ -3,17 +3,19 @@ local function listen()
     while true do
         local targetID, targetMsg, targetProt = rednet.receive()
         if targetID then
-            print("ID: " .. targetID .. "   MSG: " .. targetProt .. "   PRT: " .. targetProt)
+            print("ID: " .. targetID .. "   MSG: " .. targetMsg .. "   PRT: " .. targetProt)
         end
         sleep(1)
     end
 end
 
 while true do
-    io.write("kali@:~ ")
+    textutils.slowPrint.io.write("kali@:~ ")
     local input = read()
     if input then
         if input == "netS" then
+            shell.run("clear")
+            textutils.slowPrint.io.write("Listening for any broadcast...")
             listen()
         end
     end
