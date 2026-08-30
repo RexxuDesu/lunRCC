79759e1c5b35ae3c3d6cd02a4492a7b1d0cb845c
Rexxu <130981329+RexxuDesu@users.noreply.github.com>
2026-08-30 23:25:34 +0800

    Adds several script and changed the name of startup.lua

---
+++ release/server/subserver/itemVault.lua
@@ -0,0 +1,29 @@
+rednet.open("top")
+local var = {
+    v = peripheral.find("inventory"),
+    capacity = 0,
+    contents = 0
+}
+local function read()
+    var.capacity = 0
+    var.contents = 0
+    for slot = 1, var.v.size() do
+        var.capacity = var.capacity + var.v.getItemLimit(slot)
+        local item = var.v.getItemDetail(slot)
+        if item then
+            var.contents = var.contents + item.count
+        end
+    end
+end
+local function send()
+    local data = {
+        a = var.contents,
+        b = var.capacity
+    }
+    rednet.send(67, data, "sensor")
+end
+while true do
+    read()
+    send()
+    sleep(1)
+end
---
+++ release/server/subserver/itemVaultMain.lua
@@ -0,0 +1,45 @@
+rednet.open("front")
+local m = peripheral.find("monitor")
+m.setTextScale(2)
+local vaults = {
+    [68] = "Woods",
+    [69] = "Blocks",
+    [70] = "Placeables",
+    [71] = "Ingots",
+    [72] = "Foods",
+    [73] = "Plants",
+    [74] = "Misc"
+}
+local order = {68, 69, 70, 71, 72, 73, 74}
+local data = {}
+local function display()
+    m.clear()
+    local line = 1
+    for _, id in ipairs(order) do
+        local name = vaults[id]
+        local vault = data[id]
+        m.setCursorPos(1, line)
+        if vault then
+            local contents = vault.a
+            local capacity = vault.b
+            local percent = contents / capacity
+            m.write(name .. ": " .. contents .. "/" .. capacity)
+            line = line + 1
+            m.setCursorPos(1, line)
+            local barWidth = 20
+            local filled = math.floor(percent * barWidth)
+            m.write("[")
+            m.write(string.rep("#", filled))
+            m.write(string.rep("-", barWidth - filled))
+            m.write("] " .. string.format("%.1f%%", percent * 100))
+        else
+            m.write(name .. ": Waiting...")
+        end
+        line = line + 2
+    end
+end
+while true do
+    local senderID, message = rednet.receive("sensor")
+    data[senderID] = message
+    display()
+end
