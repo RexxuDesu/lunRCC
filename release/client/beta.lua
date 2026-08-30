c07ce6d3a44bf834dd824889d4c8da3b0525f0a4
Rexxu <130981329+RexxuDesu@users.noreply.github.com>
2026-08-30 20:43:50 +0800

    integrated update() func to the command table

--- release/client/beta.lua
+++ release/client/beta.lua
@@ -34,73 +34,70 @@ local commands = {
         shell.run("clear")
     end,
     ["update"] = function()
-        update()
+        io.write("Checking for updates...")
+        local suc, err = shell.run("wget " .. link.vers .. " " .. path.latVers)
+        if not suc then
+            term.setTextColor(colors.red)
+            print("Failed to fetch version info ", err)
+            term.setTextColor(colors.white)
+        end
+        if fs.exists(path.latVers) then
+            local file = fs.open(path.latVers, "r")
+            if file then
+                local latVers = file.readLine()
+                file.close()
+                fs.delete(path.latVers)
+                file = fs.open(path.curVers, "r")
+                local curVers = file.readLine()
+                file.close()
+                latVers = latVers:match("^%s*(.-)%s*$")
+                curVers = curVers:match("^%s*(.-)%s*$")
+                if latVers ~= curVers then
+                    print("Latest version available: " .. latVers .. ". Current version: " .. curVers)
+                    term.setTextColor(colors.green)
+                    io.write("Do you want to proceed with the update? (y/n): ")
+                    term.setTextColor(colors.white)
+                    local proceed = read()
+                    if proceed:lower() == "y" then
+                        io.write("Updating...")
+                        shell.run("rm startup")
+                        local suc, err = shell.run("wget " .. link.update .. " startup")
+                        if suc then
+                            file = fs.open(path.curVers, "w")
+                            file.write(latVers)
+                            file.close()
+                            term.setTextColor(colors.green)
+                            print("Updated to version " .. latVers)
+                            term.setTextColor(colors.white)
+                            io.write("Rebooting in 2s...")
+                            sleep(2)
+                            os.reboot()
+                        else
+                            term.setTextColor(colors.red)
+                            print("Failed to update script: ", err)
+                            term.setTextColor(colors.white)
+                        end
+                    else
+                        term.setTextColor(colors.red)
+                        io.write("Update aborted.")
+                        term.setTextColor(colors.white)
+                    end
+                else
+                    term.setTextColor(colors.green)
+                    io.write("No current updates available.")
+                    term.setTextColor(colors.white)
+                end
+            else
+                term.setTextColor(colors.red)
+                print("Failed to read file.")
+                term.setTextColor(colors.white)
+            end
+        end
     end,
     ["test"] = function()
         io.write(var.user .. "@:~$ Hello!\n")
     end
 }
-local function update()
-    io.write("Checking for updates...")
-    local suc, err = shell.run("wget " .. link.vers .. " " .. path.latVers)
-    if not suc then
-        term.setTextColor(colors.red)
-        print("Failed to fetch version info ", err)
-        term.setTextColor(colors.white)
-    end
-    if fs.exists(path.latVers) then
-        local file = fs.open(path.latVers, "r")
-        if file then
-            local latVers = file.readLine()
-            file.close()
-            fs.delete(path.latVers)
-            file = fs.open(path.curVers, "r")
-            local curVers = file.readLine()
-            file.close()
-            latVers = latVers:match("^%s*(.-)%s*$")
-            curVers = curVers:match("^%s*(.-)%s*$")
-            if latVers ~= curVers then
-                print("Latest version available: " .. latVers .. ". Current version: " .. curVers)
-                term.setTextColor(colors.green)
-                io.write("Do you want to proceed with the update? (y/n): ")
-				term.setTextColor(colors.white)
-                local proceed = read()
-                if proceed:lower() == "y" then
-                    io.write("Updating...")
-                    shell.run("rm startup")
-                    local suc, err = shell.run("wget " .. link.update .. " startup")
-                    if suc then
-                        file = fs.open(path.curVers, "w")
-                        file.write(latVers)
-                        file.close()
-                        term.setTextColor(colors.green)
-                        print("Updated to version " .. latVers)
-						term.setTextColor(colors.white)
-                        io.write("Rebooting in 2s...")
-                        sleep(2)
-                        os.reboot()
-                    else
-                        term.setTextColor(colors.red)
-                        print("Failed to update script: ", err)
-						term.setTextColor(colors.white)
-                    end
-                else
-                    term.setTextColor(colors.red)
-                    io.write("Update aborted.")
-                    term.setTextColor(colors.white)
-                end
-            else
-                term.setTextColor(colors.green)
-                io.write("No current updates available.")
-                term.setTextColor(colors.white)
-            end
-        else
-            term.setTextColor(colors.red)
-            print("Failed to read file.")
-			term.setTextColor(colors.white)
-        end
-    end
-end
 local function checkFiles()
     if not fs.exists(path.curVers) then
         local file = fs.open(path.curVers, "w")
