2320a5ec5f11eba4aded385514969cd5fed567bb
Rexxu <130981329+RexxuDesu@users.noreply.github.com>
2026-09-24 07:28:25 +0800

    Fixed stasis scripts (I forgot to add rednet.open

--- release/server/subserver/stasisChamber/stasisPuller.lua
+++ release/server/subserver/stasisChamber/stasisPuller.lua
@@ -1,12 +1,12 @@
+if not peripheral.find("modem") then io.write("Unable to find modem. Quitting in 2s...\n") sleep (2) os.reboot()
+else rednet.open(peripheral.getName(peripheral.find("modem"))) end
 local server = 0
 local function pull()
     redstone.setOutput("front", true)
-    sleep(0.5)
+    sleep(0.1)
     redstone.setOutput("front", false)
-    sleep(0.5)
+    sleep(0.1)
     redstone.setOutput("front", true)
-    sleep(0.5)
-    redstone.setOutput("front", false)
 end
 while true do
     local ID, packet = rednet.receive()
--- release/server/subserver/stasisChamber/stasisServer.lua
+++ release/server/subserver/stasisChamber/stasisServer.lua
@@ -1,16 +1,14 @@
+if not peripheral.find("modem") then io.write("Unable to find modem. Quitting in 2s...\n") sleep (2) os.reboot()
+else rednet.open(peripheral.getName(peripheral.find("modem"))) end
 local puller = {0, 0, 0}
 local wls = {7, 85, 0, 0}
 while true do
     local ID, packet = rednet.receive()
-    for _, wl in ipairs(wls) do
-        if ID == wl then
-            if ID == wls[1] and packet == "1" or ID == wls[2] and packet == "1" then
-                rednet.send(puller[1], "1")             
-            elseif ID == wls[3] and packet == "1" then
-                rednet.send(puller[2], "1")
-            elseif ID == wls[4] and packet == "1" then
-                rednet.send(puller[3], "1")
-            end
-        end
+    if ID == wls[1] and packet == "1" or ID == wls[2] and packet == "1" then
+        rednet.send(puller[1], "1")             
+    elseif ID == wls[3] and packet == "1" then
+        rednet.send(puller[2], "1")
+    elseif ID == wls[4] and packet == "1" then
+        rednet.send(puller[3], "1")
     end
 end
